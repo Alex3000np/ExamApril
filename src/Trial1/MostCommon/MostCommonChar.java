e58aa3b55343a9f8683b46377b1ae5f45ee3d90d
Alex3000np <novsiv@gmail.com>
2021-03-15 22:27:12 +0100

    trial1 chars

--- src/Trial1/MostCommon/MostCommonChar.java
+++ src/Trial1/MostCommon/MostCommonChar.java
@@ -8,39 +8,51 @@ import java.util.HashMap;
 
 public class MostCommonChar {
     public static void main(String[] args) throws Exception {
-        mostCommon("src/Trial1/MostCommon/sourceFile.txt");
+        System.out.println(mostCommon("src/Trial1/MostCommon/sourceFile.txt"));
     }
 
     //reading
     public static HashMap<Character, Integer> mostCommon(String filepath) throws Exception {
-        Path userInput = Paths.get(filepath);
-        String origText;
+        Path userInput = Paths.get(filepath);                            //reading
         String stringText;
-        int counter = 0;
-        HashMap<Character, Integer> myMap = new HashMap<>();        //empty map will be populated
+
         try {
             stringText = Files.readString(userInput);
         } catch (IOException e) {
             throw new Exception("Ty pidor");
         }
-        char[] arr = stringText.toCharArray();
-        for (char x : arr) {
-            if (myMap.containsKey(x)) {                             //not clear for me
-                counter = myMap.get(x);                             //map.get() returns value of key x
-                myMap.put(x, counter + 1);
+
+        HashMap<Character, Integer> collectMap = new HashMap<>();        //empty map will be populated <K,V>
+        HashMap<Character, Integer> resultMap = new HashMap<>();
+        int counter;
+        char[] fileTextArray = stringText.toCharArray();                 //turn string to char array
+
+        for (char x : fileTextArray) {                                   //iterating trough an array
+            if (collectMap.containsKey(x)) {                             //starts to check empty map and populates it
+                counter = collectMap.get(x);                             //map.get() returns VALUE of KEY x
+                collectMap.put(x, counter + 1);                            //populating initially empty map
             } else {
-                myMap.put(x, 1);
+                collectMap.put(x, 1);                                    //otherwise its first appearance of a character
             }
         }
-        return null;
+        //we create function getMax first, see below
+        resultMap.put(getMax(collectMap), collectMap.get(getMax(collectMap)));      //put (maxChar KEY, VALUE of maxChar) tu resultMap
+        collectMap.remove(getMax(collectMap), collectMap.get(getMax(collectMap)));   //removing first most common pair
+        resultMap.put(getMax(collectMap), collectMap.get(getMax(collectMap)));      //again put
+
+        return resultMap;
     }
 
-
-    public static Integer getMax(HashMap<Character, Integer> mapX) {
-        for (Integer z : mapX.get(z)) {
-
+    public static char getMax(HashMap<Character, Integer> mapX) {
+        int maxValue = 0;
+        char maxChar = 0;
+        for (char z : mapX.keySet()) {                                    //iterating trough KEYS in map, (keySet() returns a set of KEYS of the hash map.)
+            if (mapX.get(z) > maxValue) {                                     //if key VALUE > maxValue...
+                maxChar = z;
+                maxValue = mapX.get(z);
+            }
         }
-        return null;
+        return maxChar;
     }
 
 }
